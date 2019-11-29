e9c7eab7c523c98af7f2b96caeae4f835b88d762
Ivan Gunko <ivanfakovgun@gmail.com>
2019-11-30 00:33:20 +0300

    add aliases for `pass-git`; add gitignore for pass-update folder

---
+++ bashrc.d/alias-pass.sh
@@ -0,0 +1,3 @@
+alias pass-pull="pass git checkout master && pass git fetch --all && pass git rebase origin/master && pass git pull origin master"
+alias pass-send="pass git checkout master && pass git fetch --all && pass git rebase origin/master && pass git push origin master"
+
