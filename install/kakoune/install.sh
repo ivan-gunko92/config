ff1a049ccab50ade09cd7bf53dbbabbb6876ae15
Ivan Gunko <ivanfakovgun@gmail.com>
2019-12-03 12:34:08 +0300

    add kakoune install; start writing sync-all.sh

---
+++ install/kakoune/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu -o pipefail
+set -f
+readonly DIR=$(dirname "$0")
+#END
+CHECKOUT="v2019.07.01"
+sudo apt install libncursesw5-dev pkg-config
+git clone --branch "${CHECKOUT}" --depth 1 https://github.com/mawww/kakoune.git repo
+cd repo/src/
+make all -j $(($(nproc) + 1))
+PREFIX=$HOME/.local make install
+
