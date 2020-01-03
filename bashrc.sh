08951316562246804c64bc3dd305b1234c1b9d9d
Ivan Gunko <ivanfakovgun@gmail.com>
2020-01-03 23:50:06 +0300

    add default `less` flags in bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -19,6 +19,9 @@ export data="${my}"/data
 export music="${data}"/music
 export tips="${my}"/tips
 export PASSWORD_STORE_DIR="${my}"/pass
+
+export PAGER='less'
+export LESS='--RAW-CONTROL-CHARS --LONG-PROMPT --IGNORE-CASE --no-init --tabs=4'
 export PATH="${HOME}/bin:${HOME}/.local/bin:${config}/bin:${PATH}"
 
 echo Lol
