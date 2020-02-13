b26d7c1d4b1f83092d04f2fe48faac1746d5dc20
Ivan Gunko <ivanfakovgun@gmail.com>
2020-02-13 18:44:51 +0300

    improve Bash Prompt

--- bashrc.sh
+++ bashrc.sh
@@ -26,4 +26,30 @@ export PAGER='less'
 export LESS='--RAW-CONTROL-CHARS --LONG-PROMPT --IGNORE-CASE --tabs=4'
 export PATH="${config}/bin:${HOME}/bin:${HOME}/.local/bin:${PATH}"
 
+NC='\e[0m'
+DarkGray='\e[1;30m'
+Blue='\e[0;34m'
+LightBlue='\e[1;34m'
+Green='\e[0;32m'
+LightGreen='\e[1;32m'
+Cyan='\e[0;36m'
+LightCyan='\e[1;36m'
+Red='\e[0;31m'
+LightRed='\e[1;31m'
+Purple='\e[0;35m'
+LightPurple='\e[1;35m'
+Brown='\e[0;33m'
+Yellow='\e[1;33m'
+LightGray='\e[0;37m'
+
+function ret_code_colored {
+    if [ "$?" == "0" ]; then
+        echo -e "${Green}0${NC}"
+    else
+        echo -e "${Red}$?${NC}"
+    fi
+}
+DELIMITER="________________________________________________________________________________"
+PS1="${LightCyan}${DELIMITER}${NC}\n[\`ret_code_colored\`] \w  ${Blue}\u${NC}@${Brown}\h${NC} [jobs: \j]${NC}\n> "
+
 echo Lol
